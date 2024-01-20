1751c085d1abdc3689332747d5289e4a765abaaf
Dinesh8447 <219bca04dinesh@gmail.com>
2024-01-20 18:01:41 +0530

    header complete using flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,22 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import Header from './compoents/Header'
 
 
-function App() {
-  
+function App() {  
   return (
    <Routes>
+    <Route path='/' element={<Header/>}>
+
+    
     <Route path='/home' element={<Home/>}/>
     <Route path='/dashboard' element={<Dashboard/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/projects' element={<Projects/>}/>
     <Route path='/signin' element={<Signin/>}/>
     <Route path='/signup' element={<Signup/>}/>
+    </Route>
    </Routes>
   )
 }
