c4233c0fcf1bc955a2674ff2898f4e4836657715
Dinesh8447 <219bca04dinesh@gmail.com>
2024-01-20 18:31:19 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
 const app = express()
 
 
-
+mongoose.connect(process.env.MONGODB)
+.then(()=>console.log('connect'))
+.catch(e=>console.log(e))
 
 app.listen(4000,()=>{
     console.log('running....4000')
