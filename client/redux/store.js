cba85d0d4dc79bc884161df0f9e34d6d2f3397b8
Dinesh8447 <219bca04dinesh@gmail.com>
2024-01-21 10:48:39 +0530

    redux persist

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,11 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
 import userSlice from './user/userSlice'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
+
+// persist is used for store the use data in local storage, even through refresh the web browser,this why we use persist
+
+const rootreducer = combineReducers({
+    user:userSlice,
+})
+
+const persistconfig = {
+    key:'root',
+    storage,
+    version:1
+}
 
 
+const persistedreducers = persistReducer(persistconfig,rootreducer) 
 
 
 export const store = configureStore({
-  reducer: {
-    user:userSlice
-  },
+  reducer: persistedreducers,
+  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false})
 })
+
+
+export const persistor = persistStore(store)
+
+
