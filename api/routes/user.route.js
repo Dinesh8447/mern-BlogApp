5e1c170f9288270b6e7aa1336d84f57d5c7fd337
Dinesh8447 <219bca04dinesh@gmail.com>
2024-01-23 11:17:23 +0530

    delete api complete

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {updateuser} from '../controllers/user.controller.js'
+import {updateuser,deleteuser} from '../controllers/user.controller.js'
 import { verfiyToken } from '../utils/verifitoken.js'
 
 const router = express.Router()
 
 router.put('/update/:userid',verfiyToken,updateuser)
+router.delete('/delete/:userid',verfiyToken,deleteuser)
 
 
 
